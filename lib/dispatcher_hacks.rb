9b7469df5ff7db9038b6cae0b180987daa76db1b
Josh Goebel <dreamer3@gmail.com>
2008-11-08 16:07:17 -0500

    cleaning up

--- lib/dispatcher_hacks.rb
+++ lib/dispatcher_hacks.rb
@@ -3,7 +3,6 @@ ActionController::Dispatcher.class_eval do
   # Cleanup the application by clearing out loaded classes so they can
   # be reloaded on the next request without restarting the server.
   def cleanup_application
-    puts "CLEAN UP"
 #   ActiveRecord::Base.reset_subclasses if defined?(ActiveRecord)
 #    ActiveSupport::Dependencies.clear
     # uncomment this if your using sqlite3 in development
@@ -11,14 +10,13 @@ ActionController::Dispatcher.class_eval do
   end
   
   def reload_application
-    puts "RELOAD APP"
     ActiveSupport::Dependencies.clear
 
     # Run prepare callbacks before every request in development mode
     run_callbacks :prepare_dispatch
 
     ActionController::Routing::Routes.reload
-   ActionView::TemplateFinder.reload! unless ActionView::Base.cache_template_loading
+    ActionView::TemplateFinder.reload! unless ActionView::Base.cache_template_loading
   end
   
 
