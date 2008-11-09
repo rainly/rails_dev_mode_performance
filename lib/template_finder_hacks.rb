13fe9a5330d5e76a38f10dde33a67879791c7ed0
Josh Goebel <dreamer3@gmail.com>
2008-11-09 07:29:37 -0500

    simplify our tf patch greatly

--- lib/template_finder_hacks.rb
+++ lib/template_finder_hacks.rb
@@ -1,39 +1,5 @@
 module ActionView
   class TemplateFinder
-   
-    class << self #:nodoc:
-
-      # This method is not thread safe. Mutex should be used whenever this is accessed from an instance method
-      def process_view_paths(*view_paths)
-        view_paths.flatten.compact.each do |dir|
-          next if @@processed_view_paths.has_key?(dir) and not @@force_reload
-          @@processed_view_paths[dir] = []
-
-          # 
-          # Dir.glob("#{dir}/**/*/**") reads all the directories in view path and templates inside those directories
-          # Dir.glob("#{dir}/**") reads templates residing at top level of view path
-          # 
-          (Dir.glob("#{dir}/**/*/**") | Dir.glob("#{dir}/**")).each do |file|
-            unless File.directory?(file)
-              @@processed_view_paths[dir] << file.split(dir).last.sub(/^\//, '')
-
-              # Build extension cache
-              extension = file.split(".").last
-              if template_handler_extensions.include?(extension)
-                key = file.split(dir).last.sub(/^\//, '').sub(/\.(\w+)$/, '')
-                @@file_extension_cache[dir][key] << extension
-              end
-            end
-          end
-        end
-      end
-    end
-   
-    
-  end
-end
-
-ActionView::TemplateFinder.class_eval do
 
   # make reload! impotent incase we're only using this patch
   class << self    
@@ -46,13 +12,20 @@ ActionView::TemplateFinder.class_eval do
   def file_exists_with_auto_reload?(path)
     result=file_exists_without_auto_reload?(path)
     unless result
-      @@force_reload=true
-      self.view_paths=view_paths
+      trigger_reload_of(view_paths)
       result=file_exists_without_auto_reload?(path)
     end
     result
   end
   alias_method_chain :file_exists?, :auto_reload  
+  
+  def trigger_reload_of(*view_paths)
+    view_paths.each do |dir|
+      @@processed_view_paths[dir] = []
+      @@file_extension_cache[dir] = Hash.new {|hash, key| hash[key] = []}
+    end
+    self.view_paths=view_paths
+  end
   
   # if a file that used to be in place can no longer be found
   # then we need to return false, which will coincidentally
@@ -63,4 +36,5 @@ ActionView::TemplateFinder.class_eval do
     return p if p and File.exists?(File.join(p, template_file_name))
   end  
   
+end
 end
